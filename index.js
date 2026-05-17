748750f2c873bc68d04e91370687d1f497a3f7ec
jassugamer294-hash <jassugamer294@gmail.com>
2026-05-17 22:21:50 +0530

    Create main entry point index.js with Express setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Welcome to zevorix-panel!");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Zevorix Panel' });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Zevorix Panel is running on port ${PORT}`);
+});
